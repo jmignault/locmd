f5e5a7a54414d7f1aae117267bebce2b3c844912
jmignault <john.mignault@gmail.com>
2025-08-26 09:08:33 -0400

    Filename shuffle

--- locmd.py
+++ locmd.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
+import sys
+import time
 import requests
 ## Define the path to your file
-file_path = ARGV[1]
+file_path = sys.argv[1]
 
 # Open the file and read it line by line
 with open(file_path, 'r') as file:
     for line_number, line in enumerate(file, start=1):
         # Strip newline and whitespace
         clean_line = line.strip()
-        outf = f'{clean_line}.xml'
+        out_path = f'{clean_line}.xml'
         r = requests.get(f'https://lccn.loc.gov/{clean_line}/marcxml')
-        open(outf, 'w')
-        outf.write(r.content)
+        outf = open(out_path, 'w')
+        outf.write(r.text)
         outf.close()
-        sleep(20)
+        print(f'Saving {out_path}')
+        time.sleep(15)
